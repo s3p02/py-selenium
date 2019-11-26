0d637c9dcab483793f566951484939eb5c644cd9
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2019-11-26 16:13:26 -0700

    basic complete

--- Test.py
+++ Test.py
@@ -1,3 +1,7 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome('/Users/sharan_sreesai/Documents/try-selenium/chromedriver')
+browser.get('https://selenium.dev/')
+elem = browser.find_element_by_link_text('Downloads')
+elem.get_attribute('href')
+elem.click()
